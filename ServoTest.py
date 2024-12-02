7e8d4a6d6b916797a1fb8be6c68f3768727d3508
Telonisse <161740040+Telonisse@users.noreply.github.com>
2024-12-02 14:36:52 +0100

    fzgbdcv
    dgxcgz

---
+++ ServoTest.py
@@ -0,0 +1,17 @@
+from pyfirmata import Arduino, SERVO
+from time import sleep
+
+port = '/dev/ttyACM0' 
+board = Arduino(port) 
+servo_pin = board.get_pin('d:9:s') # To initialize the pin used 
+
+pin = 9
+board.digital[pin].mode = SERVO
+
+def rotateServo(pin, angle):
+    board.digital[pin].write(angle)
+    sleep(0.015)
+    
+while True:
+    for i in range(0,180):
+        rotateServo(pin, i)
